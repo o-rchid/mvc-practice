f661b8d2fbc537fd1e90531803a94bfcaa506f9d
o-rchid <ozzve35@gmail.com>
2023-09-14 23:40:20 +0900

    경곗값 테스트, 테스트하기 쉬운 코드

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -20,9 +22,10 @@ public class PasswordValidatorTest {
     }
 
     @DisplayName("비밀번호가 최소 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
-    @Test
-    void validatePasswordTest2() {
-        assertThatCode(() -> PassWordValidator.validate("a"))
+    @ParameterizedTest
+    @ValueSource(strings = {"aabbcce", "aabbccddeeffe"})
+    void validatePasswordTest2(String password) {
+        assertThatCode(() -> PassWordValidator.validate(password))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
     }
