2c37a86f1e6d3ff2cf99c0b8d4fbd5505d0375df
o-rchid <ozzve35@gmail.com>
2023-10-15 23:37:32 +0900

    ViewResolver

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -27,4 +27,8 @@ public class User {
     public int hashCode() {
         return Objects.hash(userId, name);
     }
+
+    public String getUserId() {
+        return userId;
+    }
 }
--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -2,6 +2,9 @@ package org.example.mvc;
 
 import org.example.mvc.controller.Controller;
 import org.example.mvc.controller.RequestMethod;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +17,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
@@ -22,12 +28,16 @@ public class DispatcherServlet extends HttpServlet {
 
     private RequestMappingHandlerMapping rehm;
 
+    private List<ViewResolver> viewResolvers;
+
     @Override
     public void init() throws ServletException {
         log.info("[DispatcherServlet] init");
 
         rehm = new RequestMappingHandlerMapping();
         rehm.init();
+
+        viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
 
@@ -39,8 +49,10 @@ public class DispatcherServlet extends HttpServlet {
             Controller handler = rehm.findHandler(new HandlerKey(RequestMethod.valueOf(request.getMethod()), request.getRequestURI()));
             String viewName =  handler.handleRequest(request, response);
 
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-            requestDispatcher.forward(request, response);
+            for (ViewResolver viewResolver : viewResolvers) {
+                View view = viewResolver.resolveView(viewName);
+                view.render(new HashMap<>(), request, response);
+            }
         } catch (Exception e) {
             log.error("exception occurred: [{}]", e.getMessage(), e);
             throw new ServletException(e);
