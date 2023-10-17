a82c8e5e4a405f69785a6a11f746ac7a03f9bcff
o-rchid <ozzve35@gmail.com>
2023-10-17 21:02:12 +0900

    MVC 최종

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -35,11 +35,12 @@ public class DispatcherServlet extends HttpServlet {
         RequestMappingHandlerMapping rmhm = new RequestMappingHandlerMapping();
         rmhm.init();
 
-        AnnotationHandlerMapping ahm = new AnnotationHandlerMapping();
+        AnnotationHandlerMapping ahm = new AnnotationHandlerMapping("org.example");
+        ahm.init();
 
         handlerMappings = List.of(rmhm, ahm);
 
-        handlerAdapters = List.of(new SimpleControllerHandlerAdapter());
+        handlerAdapters = List.of(new SimpleControllerHandlerAdapter(), new AnnotationHandlerAdapter());
         viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
--- src/main/java/org/example/mvc/controller/HomeController.java
+++ src/main/java/org/example/mvc/controller/HomeController.java
@@ -1,12 +1,13 @@
 package org.example.mvc.controller;
 
 
+import org.example.mvc.annotation.Controller;
 import org.example.mvc.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@org.example.annotation.Controller
+@Controller
 public class HomeController{
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
